e4e33e2586af2ea9b2d053ee0524568c8063b0e9
gmelan <gmelanals@gmail.com>
2023-07-17 15:57:14 +0900

    inital commit

---
+++ src/main/java/com/dashboard/gmelan/GmelanApplication.java
@@ -0,0 +1,13 @@
+package com.dashboard.gmelan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmelanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmelanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dashboard/gmelan/GmelanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dashboard.gmelan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmelanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
