4295c9e0f8a4595752743ba5610b2da150fc3349
gmelan <gmelanals@gmail.com>
2023-10-13 15:51:41 +0900

    modified some name errors

--- src/main/java/com/dashboard/gmelan/notice/controller/NoticeController.java
+++ src/main/java/com/dashboard/gmelan/notice/controller/NoticeController.java
@@ -2,12 +2,16 @@ package com.dashboard.gmelan.notice.controller;
 
 import com.dashboard.gmelan.notice.entity.NoticeEntity;
 import com.dashboard.gmelan.notice.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 
+@Controller
 public class NoticeController {
+    @Autowired
     private NoticeService noticeService;
     /**
      * Get notice page.
