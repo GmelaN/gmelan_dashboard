4bccc8e482672e886e1d06fb5fe5eeb1b5f3653c
gmelan <gmelanals@gmail.com>
2024-05-14 11:33:26 +0900

    refactor: 사용되지 않는 임포트 구문 제거

--- src/main/java/com/dashboard/gmelan/GmelanApplication.java
+++ src/main/java/com/dashboard/gmelan/GmelanApplication.java
@@ -2,7 +2,6 @@ package com.dashboard.gmelan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class GmelanApplication {
